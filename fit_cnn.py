16c732a5bf42b7439f7f24820f500cc0feea057b
Danila <danilaeremenko@mail.ru>
2022-10-10 19:37:02 +0300

    HP: first correctly working NN fitting pipeline updated

--- fit_cnn.py
+++ fit_cnn.py
@@ -1,8 +1,10 @@
+import random
 from typing import Tuple, List
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision import transforms
 from torch.utils.data import Dataset
 import torch.optim as optim
 import torchmetrics
@@ -51,20 +53,27 @@ def get_data() -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
         for snapshot in class_snapshots
     ]
 
-    max_y = max([tup[2].shape[1] for tup in interm_list])
-    max_x = max([tup[2].shape[2] for tup in interm_list])
+    filt_interm_list = [tup for tup in interm_list if tup[2].shape[1] < 70 and tup[2].shape[2] < 70]
 
-    x_data = np.zeros(shape=(len(interm_list), 23, max_y, max_x), dtype='uint8')
+    print(f"{len(interm_list) - len(filt_interm_list)} snapshots filtered cause of size")
 
-    for i, tup in enumerate(interm_list):
-        x_data[i] = np.resize(tup[2], (23, max_y, max_x))
+    # data padding
+    max_y = max([tup[2].shape[1] for tup in filt_interm_list])
+    max_x = max([tup[2].shape[2] for tup in filt_interm_list])
 
+    channels_num = 23
+    x_data = np.zeros(shape=(len(filt_interm_list), channels_num, max_y, max_x))
+    for i, tup in enumerate(filt_interm_list):
+        curr_img = tup[2]
+        x_data[i, :, :curr_img.shape[1], :curr_img.shape[2]] = curr_img
+
+    # slice range
     x_data = x_data[:, 10:13]
 
-    y_data = np.array([0 if 'health' in tup[1] else 1 for tup in interm_list])
+    y_data = np.array([0 if 'health' in tup[1] else 1 for tup in filt_interm_list])
     y_data = np.expand_dims(y_data, 1)
 
-    x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.33, random_state=42)
+    x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, shuffle=True, test_size=0.25, random_state=42)
 
     return x_train, y_train, x_test, y_test
 
@@ -74,6 +83,9 @@ class HyperDataset(Dataset):
         self.x_data = torch.from_numpy(x_data).float()
         self.y_data = torch.from_numpy(y_data)
         self.y_data = self.y_data.long() if y_to_long else self.y_data.float()
+
+        self.x_data = torch.nn.functional.normalize(self.x_data)
+
         self.len = len(self.x_data)  # Size of data
         self.device = device
 
@@ -84,30 +96,50 @@ class HyperDataset(Dataset):
         return self.len
 
 
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, kernel_size: int):
+        super().__init__()
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size, kernel_size)
+
+    def forward(self, x):
+        return self.pool(F.relu(self.conv(x)))
+
+
 class Net(nn.Module):
     def __init__(self, channels_num: int, y_size: int, x_size: int):
         print(f"Building cnn for data with shape = [{channels_num}, {y_size}, {x_size}]")
         super().__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=6, kernel_size=3)
-        self.pool1 = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=3)
-        self.pool2 = nn.MaxPool2d(2, 2)
 
-        for i in range(2):
-            x_size = x_size // 2 - 1
-            y_size = y_size // 2 - 1
+        self.conv_layers = nn.ModuleList(
+            [
+                ConvBlock(in_channels=3, out_channels=8, kernel_size=3),
+                ConvBlock(in_channels=8, out_channels=16, kernel_size=3),
+                # ConvBlock(in_channels=16, out_channels=32, kernel_size=3)
+            ]
+        )
 
-        self.fc1 = nn.Linear(int(x_size * y_size * 16), 120)
+        for conv_layer in self.conv_layers:
+            x_size //= conv_layer.kernel_size
+            y_size //= conv_layer.kernel_size
+
+        out_channels = self.conv_layers[-1].out_channels if len(self.conv_layers) != 0 else channels_num
+
+        self.dropout = nn.Dropout(p=0.25)
+
+        self.fc1 = nn.Linear(int(x_size * y_size * out_channels), 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 1)
 
     def forward(self, x):
-        x = self.pool1(F.relu(self.conv1(x)))
-        x = self.pool2(F.relu(self.conv2(x)))
+        for i, conv_layer in enumerate(self.conv_layers):
+            x = conv_layer(x)
         x = torch.flatten(x, 1)  # flatten all dimensions except batch
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = torch.sigmoid(self.fc3(x))
+        x = torch.relu(self.fc1(self.dropout(x)))
+        x = torch.relu(self.fc2(self.dropout(x)))
+        x = self.fc3(self.dropout(x))
         return x
 
 
@@ -138,6 +170,24 @@ def draw_history():
     fig.show()
 
 
+classes = {0: 'health', 1: 'phyto'}
+
+
+def draw_predicts(ex_num: int = 4):
+    fig, axes = plt.subplots(nrows=ex_num, ncols=1)
+    for ax in axes:
+        batch_image, batch_label = random.choice(testloader.dataset)
+        ax.imshow(batch_image.cpu().detach().numpy()[0])
+        ax.set_title(classes[int(batch_label)])
+    fig.tight_layout()
+    fig.show()
+
+
+def get_random_predict():
+    tup = random.choice(testloader.dataset)
+    return torch.sigmoid(net(tup[0].unsqueeze(0))), tup[1]
+
+
 def fit(epoch_num: int, train_history: List[EpochMetrics], test_history: List[EpochMetrics]):
     assert len(train_history) == len(test_history)
 
@@ -152,6 +202,7 @@ def fit(epoch_num: int, train_history: List[EpochMetrics], test_history: List[Ep
         for i, data in enumerate(trainloader):
             # get the inputs; data is a list of [inputs, labels]
             inputs, labels = data
+            inputs = transforms(inputs)
 
             # zero the parameter gradients
             optimizer.zero_grad()
@@ -163,9 +214,9 @@ def fit(epoch_num: int, train_history: List[EpochMetrics], test_history: List[Ep
             optimizer.step()
 
             train_loss_sum += loss.item()
-            train_acc_sum += acc_metric(outputs.int(), labels.int()).item()
-            train_f1_sum += f1_metric(outputs.int(), labels.int()).item()
-            train_matrix += confusion_metric(outputs.int(), labels.int()).cpu().detach().numpy()
+            train_acc_sum += acc_metric(torch.sigmoid(outputs).int(), labels.int()).item()
+            train_f1_sum += f1_metric(torch.sigmoid(outputs).int(), labels.int()).item()
+            train_matrix += confusion_metric(torch.sigmoid(outputs).int(), labels.int()).cpu().detach().numpy()
 
         train_history.append(
             EpochMetrics(
@@ -185,17 +236,19 @@ def fit(epoch_num: int, train_history: List[EpochMetrics], test_history: List[Ep
 
             # forward + backward + optimize
             with torch.no_grad():
+                inputs = transforms(inputs)
                 outputs = net(inputs)
+
                 test_loss_sum += criterion(outputs, labels).item()
-                test_acc_sum += acc_metric(outputs.int(), labels.int()).item()
-                test_f1_sum += f1_metric(outputs.int(), labels.int()).item()
-                test_matrix += confusion_metric(outputs.int(), labels.int()).cpu().detach().numpy()
+                test_acc_sum += acc_metric(torch.sigmoid(outputs).int(), labels.int()).item()
+                test_f1_sum += f1_metric(torch.sigmoid(outputs).int(), labels.int()).item()
+                test_matrix += confusion_metric(torch.sigmoid(outputs).int(), labels.int()).cpu().detach().numpy()
 
         test_history.append(
             EpochMetrics(
-                loss=test_loss_sum / len(trainloader),
-                acc=test_acc_sum / len(trainloader),
-                f1=test_f1_sum / len(trainloader),
+                loss=test_loss_sum / len(testloader),
+                acc=test_acc_sum / len(testloader),
+                f1=test_f1_sum / len(testloader),
                 confusion_matrix=test_matrix
             )
         )
@@ -208,24 +261,33 @@ def fit(epoch_num: int, train_history: List[EpochMetrics], test_history: List[Ep
 
 if __name__ == '__main__':
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print(f"{device} device choose")
 
     x_data_train, y_data_train, x_data_test, y_data_test = get_data()
 
     batch_size = 4
 
+    transforms = torch.nn.Sequential(
+        # transforms.RandomRotation(degrees=90),
+        # transforms.RandomVerticalFlip(p=0.5)
+        # transforms.RandomErasing()
+    )
+
     trainloader = torch.utils.data.DataLoader(
         dataset=HyperDataset(x_data=x_data_train, y_data=y_data_train, device=device),
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=0,
+
+    )
+
+    testloader = torch.utils.data.DataLoader(
+        dataset=HyperDataset(x_data=x_data_test, y_data=y_data_test, device=device),
         batch_size=batch_size,
         shuffle=True,
         num_workers=0
     )
 
-    testloader = torch.utils.data.DataLoader(
-        dataset=HyperDataset(x_data=x_data_train, y_data=y_data_train, device=device),
-        batch_size=batch_size,
-        shuffle=True,
-        num_workers=0
-    )
     print(f"train size = {len(trainloader.dataset)} snapshots")
     print(f"test size = {len(testloader.dataset)} snapshots")
     classes = ('health', 'phyto')
@@ -233,14 +295,20 @@ if __name__ == '__main__':
     net = Net(*x_data_train.shape[1:])
     net.to(device=device)
 
-    criterion = nn.BCELoss().to(device)
+    criterion = nn.BCEWithLogitsLoss().to(device)
     acc_metric = torchmetrics.Accuracy(num_classes=2).to(device)
     f1_metric = torchmetrics.F1Score(num_classes=2).to(device)
     confusion_metric = torchmetrics.ConfusionMatrix(num_classes=2).to(device)
 
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    optimizer = optim.Adam(
+        net.parameters(),
+        lr=0.001
+    )
 
     train_history = []
     test_history = []
 
     fit(epoch_num=10, train_history=train_history, test_history=test_history)
+
+    get_random_predict()
+    draw_predicts(ex_num=4)
