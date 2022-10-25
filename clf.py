5bda7a6e9f9886820c8b198216453dca97c9c67f
Danila <danilaeremenko@mail.ru>
2022-10-25 16:39:14 +0300

    HP: clf_args argument added into clf_build(..) function

--- clf.py
+++ clf.py
@@ -1,10 +1,11 @@
-from typing import List, Dict
+from typing import List, Dict, Optional
 
 import matplotlib.pyplot as plt
 
 from sklearn import preprocessing, tree, metrics
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score, train_test_split
+from sklearn.pipeline import make_pipeline
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 
@@ -20,20 +21,28 @@ def clf_visualize(clf, x_keys: List[str], class_labels: List[str]):
         pass  # TODO implement
 
 
-def clf_build(features_df: pd.DataFrame, x_keys: List[str], y_key: str, method_name='lr') -> Dict[str, float]:
+def clf_build(
+        features_df: pd.DataFrame,
+        x_keys: List[str],
+        y_key: str,
+        method_name='lr',
+        clf_args: Optional[dict] = None
+) -> Dict[str, float]:
+    clf_args = {} if clf_args is None else clf_args
+
     x_all = features_df[x_keys]
     y_all = features_df[y_key]
 
     scaler = preprocessing.StandardScaler().fit(x_all)
 
-    x_train, x_test, y_train, y_test = train_test_split(scaler.transform(x_all), y_all, test_size=0.33, random_state=42)
+    x_train, x_test, y_train, y_test = train_test_split(x_all, y_all, test_size=0.33, random_state=42)
 
     if method_name == 'lr':
-        clf = LogisticRegression(random_state=16)
+        clf = make_pipeline(scaler, LogisticRegression(**clf_args, random_state=16))
     elif method_name == 'dt':
-        clf = DecisionTreeClassifier(random_state=16)
+        clf = make_pipeline(scaler, DecisionTreeClassifier(**clf_args, random_state=16))
     elif method_name == 'svc':
-        clf = SVC(random_state=16)
+        clf = make_pipeline(scaler, SVC(**clf_args, random_state=16))
     else:
         raise Exception(f"Undefined clf method = {method_name}")
 
@@ -60,6 +69,6 @@ def clf_build(features_df: pd.DataFrame, x_keys: List[str], y_key: str, method_n
             *metrics.roc_curve(y_score=str_to_int(y_test_pred), y_true=str_to_int(y_test), pos_label=2)[:2]
         ).__round__(2),
 
-        'cross_val': cross_val_score(clf, scaler.transform(x_all), y_all, cv=5).round(2),
+        'cross_val': cross_val_score(clf, x_all, y_all, cv=5).round(2),
         'clf': clf
     }
