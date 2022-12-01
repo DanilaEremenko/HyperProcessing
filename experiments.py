45ac83c558ca2be10209fab2c4c0d6f35c1d0d49
Danila <danilaeremenko@mail.ru>
2022-12-01 22:41:11 +0300

    HP: WHEAT_ALL_JUSTIFIED_EXP variable added into experiments.py

--- experiments.py
+++ experiments.py
@@ -110,6 +110,26 @@ WHEAT_ALL_CLEAR_EXP = {
     ]
 }
 
+WHEAT_ALL_JUSTIFIED_EXP = {
+    'health2': [
+        f'csv/sep-wheat-2/wheat-control/wheat-control-{i}_000'
+        for i in [0, 2, 3]
+    ],
+    'puccinia phyto2': [
+        f'csv/sep-wheat-2/wheat-puccinia/wheat-puccinia-{i}_000'
+        for i in [4, 5, 6]
+    ],
+
+    'health3': [
+        f'csv/sep-wheat-3/wheat-control/wheat_day{i}_control_000'
+        for i in [4, 5, 6]
+    ],
+    'puccinia phyto3': [
+        f'csv/sep-wheat-3/wheat-puccinia/wheat_day{i}_experiment_000'
+        for i in [4, 5, 6]
+    ]
+}
+
 DYNAMIC_CHECK = {
     # **{
     #     f'puccinia day {i}': [f'csv/sep-wheat-1/wheat-puccinia/wheat-puccinia-{i}']
@@ -120,24 +140,24 @@ DYNAMIC_CHECK = {
     #     for i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
     # },
 
+    **{
+        f'puccinia day {i}': [f'csv/sep-wheat-2/wheat-puccinia/wheat-puccinia-{i}_000']
+        for i in [0, 2, 3, 4, 5, 6, 7, 8]
+    },
+
+    **{
+        f'health day {i}': [f'csv/sep-wheat-2/wheat-control/wheat-control-{i}_000']
+        for i in [0, 2, 3, 4, 5, 6, 7, 8]
+    },
+
     # **{
-    #     f'puccinia day {i}': [f'csv/sep-wheat-2/wheat-puccinia/wheat-puccinia-{i}_000']
-    #     for i in [0, 2, 3, 4, 5, 6, 7, 8]
+    #     f'puccinia day {i}': [f'csv/sep-wheat-3/wheat-puccinia/wheat_day{i}_experiment_000']
+    #     for i in [4, 5, 6, 7, 8]
     # },
     #
     # **{
-    #     f'health day {i}': [f'csv/sep-wheat-2/wheat-control/wheat-control-{i}_000']
-    #     for i in [0, 2, 3, 4, 5, 6, 7, 8]
+    #     f'health day {i}': [f'csv/sep-wheat-3/wheat-control/wheat_day{i}_control_000']
+    #     for i in [4, 5, 6, 7, 8]
     # },
-
-    **{
-        f'puccinia day {i}': [f'csv/sep-wheat-3/wheat-puccinia/wheat_day{i}_experiment_000']
-        for i in [4, 5, 6, 7, 8]
-    },
-
-    **{
-        f'health day {i}': [f'csv/sep-wheat-3/wheat-control/wheat_day{i}_control_000']
-        for i in [4, 5, 6, 7, 8]
-    },
 
 }
