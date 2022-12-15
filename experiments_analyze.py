181b7a22c718199d31371c049fea2602c2e027b3
Danila <danilaeremenko@mail.ru>
2022-12-15 12:11:51 +0300

    HP: experiments_analyze.py updated

--- experiments_analyze.py
+++ experiments_analyze.py
@@ -5,6 +5,12 @@ import pandas as pd
 from drawing import draw_hp_glasses
 from experiments import WHEAT_ALL_CLEAR_EXP, DYNAMIC_CHECK
 from snapshots_processing import parse_classes, SnapshotMeta, WLS
+import matplotlib
+
+font = {'family': 'Times New Roman',
+        'size': 28}
+matplotlib.rc('font', **font)
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -19,12 +25,13 @@ classes_features_dict = parse_classes(
     max_files_in_dir=MAX_FILES_IN_DIR
 )
 
-draw_hp_glasses(
-    all_classes=[classes_features_dict[key][0:2] for key in classes_features_dict.keys()],
-    classes_names=[key for key in classes_features_dict.keys()],
-    bname='YRI',
-    res_path=f'yellow_rust.html'
-)
+
+# draw_hp_glasses(
+#     all_classes=[classes_features_dict[key][0:2] for key in classes_features_dict.keys()],
+#     classes_names=[key for key in classes_features_dict.keys()],
+#     bname='YRI',
+#     res_path=f'yellow_rust.html'
+# )
 
 
 def agg_curve(snapshots: List[SnapshotMeta]):
@@ -33,7 +40,7 @@ def agg_curve(snapshots: List[SnapshotMeta]):
     # df.plot.hist(bins=12, alpha=0.5)
     # plt.title()
     # return np.mean([snap.bands['all'].band_data.mean(axis=0) for snap in snapshots], axis=0)
-    return np.mean([snap.bands['all'].band_data.std(axis=0) for snap in snapshots], axis=0)
+    return np.mean([snap.bands['all'].band_data.mean(axis=0) for snap in snapshots], axis=0)
 
 
 # for class_name, color in zip(['health2', 'puccinia phyto2'], ['#96E637', '#FF9999']):
@@ -74,13 +81,14 @@ fig, axes = plt.subplots(ncols=len(days), nrows=1, figsize=(len(days) * 9, 6))
 for day, ax in zip(days, axes):
     health_curve = agg_curve(classes_features_dict[f'health day {day}'])
     puccina_curve = agg_curve(classes_features_dict[f'puccinia day {day}'])
-    ax.plot(WLS, health_curve, color='#96E637', label='health')
-    ax.plot(WLS, puccina_curve, color='#FF9999', label='puccina')
+    ax.plot(WLS, health_curve, color='#96E637', label='health', linewidth=4)
+    ax.plot(WLS, puccina_curve, color='#FF9999', label='puccina', linewidth=4)
     ax.legend(loc="lower right")
     ax.set_title(f'day {day}')
-    # ax.set_ylim(4000, 11_000)
-    ax.set_ylim(0, 1800)
+    ax.set_ylim(4000, 11_000)
+    # ax.set_ylim(0, 1800)
 
 plt.tight_layout()
 
+plt.savefig('topic/exp3_days_fair.png')
 plt.show()
